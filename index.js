b0f907b3770105868b84b844e83b803e172423f0
Diksha Gupta <dikshagupta2012@gmail.com>
2020-09-25 13:58:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.static(__dirname+'/public'));
 app.use((req, res, next) => {
   res.statusCode = 200;
   res.setHeader('Content-Type','text/html');
-  res.end('<html><body><h1>Hello</h1></body></html>');
+  res.end('<html><body><h1>Hello World</h1></body></html>');
 });
 
 app.listen(port, () => {
